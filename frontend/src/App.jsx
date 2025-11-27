04fc6e2199aedf1eefbcfd05246468fe1e4b4f8e
Yatin Malhotra <yatin.malhotra@mail.utoronto.ca>
2025-11-26 21:53:20 -0500

    Edit Promotion fixed

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -113,6 +113,14 @@ function App() {
                   </ProtectedRoute>
                 }
               />
+              <Route
+                path="/promotions/:promotionId/edit"
+                element={
+                  <ProtectedRoute requiredRole="manager">
+                    <CreatePromotion />
+                  </ProtectedRoute>
+                }
+              />
 
               <Route
                 path="/users"
